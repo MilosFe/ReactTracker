8875d99fecb52b7425275c47d2b4b829c7c017da
Derek Maffett <derekgmaffett@gmail.com>
2017-11-21 00:32:54 +0100

    add styled desktop cards

--- src/components/reportList/report.js
+++ src/components/reportList/report.js
@@ -4,14 +4,31 @@ import classnames from 'classnames';
 
 import styles from './styles.css';
 
+const Meteocon = ({ className, name }) => {
+    return (
+        <img
+            className={classnames(styles.meteocon, className)}
+            src={`/public/${name}.svg`}
+        />
+    );
+};
+
 class Report extends Component {
     render() {
-        const { cardSize, color, report } = this.props;
+        const { cardSize, color, report: { location, weather } } = this.props;
 
         return (
             <div className={classnames(styles.reportContainer, color)}>
-                <img className={styles.smallIcon} src="/public/2.svg" />
-                {report.location.name}
+                <Meteocon name="2" className={styles.smallIcon} />
+                <div className={styles.detailsContainer}>
+                    <div className={styles.degreesContainer}>
+                        <span className={styles.degrees}>{weather.tempC}</span>
+                        <span className={styles.degreesMarker}>°C</span>
+                    </div>
+                    <span className={styles.cityName}>
+                        {`${location.name}, ${location.country}`}
+                    </span>
+                </div>
             </div>
         );
 
