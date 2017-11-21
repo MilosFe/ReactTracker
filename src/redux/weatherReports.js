f5b2177916b8c3f9db923c65400fab9fb48de7b8
Derek Maffett <derekgmaffett@gmail.com>
2017-11-21 22:37:23 +0100

    improved background colors

--- src/redux/weatherReports.js
+++ src/redux/weatherReports.js
@@ -1,8 +1,9 @@
 import createReducer from './createReducer';
 import { fetchCurrentWeather } from '../api';
+const reportsStub = [{"location":{"name":"Stock","country":"Germany"},"weather":{"isDay":false,"tempC":7,"windKph":0,"cloudPc":0,"precipitation":0}},{"location":{"name":"Dala Airport","country":"Sweden"},"weather":{"isDay":false,"tempC":-11,"windKph":11.2,"cloudPc":0,"precipitation":0}},{"location":{"name":"Hector International Airport","country":"United States of America"},"weather":{"isDay":true,"tempC":-4.4,"windKph":34.9,"cloudPc":50,"precipitation":0}},{"location":{"name":"Kano","country":"Nigeria"},"weather":{"isDay":false,"tempC":24,"windKph":6.8,"cloudPc":0,"precipitation":0}},{"location":{"name":"Baro","country":"Philippines"},"weather":{"isDay":false,"tempC":24.2,"windKph":6.5,"cloudPc":30,"precipitation":0}},{"location":{"name":"Stockholm","country":"Sweden"},"weather":{"isDay":true,"tempC":-1,"windKph":6.8,"cloudPc":75,"precipitation":0.1}},{"location":{"name":"Vancouver","country":"Canada"},"weather":{"isDay":false,"tempC":4,"windKph":9,"cloudPc":25,"precipitation":0}},{"location":{"name":"Montreal","country":"Canada"},"weather":{"isDay":false,"tempC":-5,"windKph":11.2,"cloudPc":100,"precipitation":0}},{"location":{"name":"London","country":"United Kingdom"},"weather":{"isDay":true,"tempC":13,"windKph":28.1,"cloudPc":75,"precipitation":0.1}},{"location":{"name":"Tabou","country":"Burkina Faso"},"weather":{"isDay":true,"tempC":26.7,"windKph":2.5,"cloudPc":16,"precipitation":0}},{"location":{"name":"Lon","country":"Burkina Faso"},"weather":{"isDay":true,"tempC":26.7,"windKph":2.5,"cloudPc":16,"precipitation":0}}];
 
 const initialState = {
-    reports: []
+    reports: reportsStub
 };
 
 const FETCH_CURRENT_WEATHER_SUCCESS = 'FETCH_CURRENT_WEATHER_SUCCESS';
